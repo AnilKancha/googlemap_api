d44a1a6431cd99ec4fc82bd581c126e93a9a8b02
ap_chochangae <banilpun@yahoo.com>
2017-01-26 14:47:41 +0545

    adding google map api

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Allenspun/Desktop/google_map/map/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ mapping/googlemap_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GooglemapApiConfig(AppConfig):
+    name = 'googlemap_api'
